ed33181c488dee59b4a6f45351eb4945fcb5693f
Sapthawee Srichomthong <Mypandacm@hotmail.co.th>
2018-03-21 00:36:03 +0700

    Add logging and add override method

--- myserver/src/main/scala/com/td/akkasocket/myserver/ProxyServerHandler.scala
+++ myserver/src/main/scala/com/td/akkasocket/myserver/ProxyServerHandler.scala
@@ -1,15 +1,42 @@
 package com.td.akkasocket.myserver
 
 import akka.actor.ActorRef
-import com.td.akkasocket.myserver.actor.ProxyControllerActor.CreateClient
+import com.td.akkasocket.myserver.actor.ProxyControllerActor.{CreateClient, RemoveClient}
+import com.typesafe.scalalogging.StrictLogging
 import io.netty.buffer.Unpooled
 import io.netty.channel.{ChannelHandlerContext, ChannelInboundHandlerAdapter}
 import io.netty.util.CharsetUtil
 
-class ProxyServerHandler(controllerActor: ActorRef) extends ChannelInboundHandlerAdapter {
+class ProxyServerHandler(controllerActor: ActorRef) extends ChannelInboundHandlerAdapter with StrictLogging {
   override def channelActive(ctx: ChannelHandlerContext): Unit = {
     ctx.writeAndFlush(Unpooled.copiedBuffer("Hello from server", CharsetUtil.UTF_8))
     controllerActor ! CreateClient(ctx)  //Create Actor when client was connected
+  }
+
+  override def channelReadComplete(ctx: ChannelHandlerContext): Unit = {
+    logger.info("channelReadComplete")
+  }
+
+  override def channelWritabilityChanged(ctx: ChannelHandlerContext): Unit = {
+    logger.info("channelWritabilityChanged")
+  }
+
+  override def channelInactive(ctx: ChannelHandlerContext): Unit = {
+    logger.info("Some one disconnected")
+  }
+
+  override def channelRead(ctx: ChannelHandlerContext, msg: scala.Any): Unit = {
+    logger.info("channelRead")
+  }
+
+  override def channelUnregistered(ctx: ChannelHandlerContext): Unit = {
+    logger.info("channelUnregistered")
+    ctx.channel().closeFuture()
+    ctx.close
+  }
+
+  override def channelRegistered(ctx: ChannelHandlerContext): Unit = {
+    logger.info("channelRegistered")
   }
 
   override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
